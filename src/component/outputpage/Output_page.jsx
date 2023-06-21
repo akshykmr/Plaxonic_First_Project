b7bb9b4fd948def525259d3a41c874b88782116e
C1PH3R502 <tech24into07@gmail.com>
2023-06-21 14:01:21 +0530

    Output page card layout fix-2

--- src/component/outputpage/Output_page.jsx
+++ src/component/outputpage/Output_page.jsx
@@ -23,56 +23,51 @@ const Output_page = ({inputImportedData}) => {
 
   return (
     <>
-   
-
-        <div className="output-page">
-            <div className="output-container">
+      <div className="output-page">
+        <div className="output-container">
+          {isOuputRecieved ? 
             <ul>
-  {cards.map((card, index)=>(
-    <li key={index}>{isOuputRecieved ? 
-            <div class="Card-Layout">
-        <div class="Card-Layout-body">
-            <div class="card-body">
-                <div class="card">
-                  <div className="card-details">
-                    <h4 class="mb-4">{inputImportedData.Car_Name}</h4>
-                    <span className="first-row">
-                      <h2>Car Model:</h2>
-                    <h5>{inputImportedData.Car_Modal}</h5>
-                    </span>
-                    <span className="first-row ">
-                    <h2>Purchase Year: </h2>
-                    <h5 class="mb-1" > {inputImportedData.Purchase_Year}</h5>
-                    </span>
-                    <span className="first-row">
-                    <h2>Transmission: </h2>
-                    <h5 class="mb-1" >{inputImportedData.Transmission}</h5>
-                    </span>
-                    <span className="first-row">
-                    <h2>Fuel Type: </h2>
-                    <h5>{inputImportedData.Fuel_Type}</h5> 
-                    </span>
+              {cards.map((card, index)=>(
+                <li key={index}>
+                  <div class="Card-Layout">
+                    <div class="Card-Layout-body">
+                      <div class="card-body">
+                        <div class="card">
+                          <div className="card-details">
+                            <h4 class="mb-4">{card.Car_Name}</h4>
+                            <span className="first-row">
+                              <h2>Car Model:</h2>
+                              <h5>{card.Car_Modal}</h5>
+                            </span>
+                            <span className="first-row ">
+                              <h2>Purchase Year: </h2>
+                              <h5 class="mb-1" > {card.Purchase_Year}</h5>
+                            </span>
+                            <span className="first-row">
+                              <h2>Transmission: </h2>
+                              <h5 class="mb-1" >{card.Transmission}</h5>
+                            </span>
+                            <span className="first-row">
+                              <h2>Fuel Type: </h2>
+                              <h5>{card.Fuel_Type}</h5> 
+                            </span>
+                          </div>
+                          <div class="img-wrap">
+                            {card.imageList.map((imageUrl, index) => (
+                              <img key={index} src={imageUrl} alt="CAR IMG" />
+                            ))}
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div class="img-wrap">
-                    {imageList.map((imageUrl, index) => (
-        <img key={index} src={imageUrl} alt={`Image ${index}`} />
-      ))}
                   </div>
-              </div>
-             
-          </div>
-      </div>
-  </div>
-  
-  
-           : 
-           <div>there is nothing</div>} </li>
-           ))} 
-           
-         </ul>        
-            </div>
+                </li>
+              ))}   
+            </ul>   
+          : 
+          <div>there is nothing</div> }      
         </div>
-       
+      </div> 
     </>
   )
 }
