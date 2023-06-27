9fb57a4070eac10dca5c61982aa7a6304f39e1b1
C1PH3R502 <tech24into07@gmail.com>
2023-06-27 12:26:15 +0530

    filter functionality added in listView page-working fine

--- src/component/outputpage/ListViewOutput.jsx
+++ src/component/outputpage/ListViewOutput.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React,{useEffect, useState} from 'react';
 import './ListViewOutput.scss';
 // import IMG from '../../assets/1.jpg'
@@ -52,15 +53,15 @@ const ListViewOutput = () => {
         }
       };
 
-    //   const handleCheckboxClickAll = () => {
-    //     if (inputImportedData && Array.isArray(inputImportedData) && selectedItems.length === inputImportedData.length) {
-    //       // If all items are already selected, clear the selected items array
-    //       setSelectedItems([]);
-    //     } else if (inputImportedData && Array.isArray(inputImportedData)) {
-    //       // Select all items by storing their index values in the selected items array
-    //       setSelectedItems([...Array(inputImportedData.length).keys()]);
-    //     }
-    //   };
+      const handleCheckboxClickAll = () => {
+        if (inputImportedData && Array.isArray(inputImportedData) && selectedItems.length === inputImportedData.length) {
+          // If all items are already selected, clear the selected items array
+          setSelectedItems([]);
+        } else if (inputImportedData && Array.isArray(inputImportedData)) {
+          // Select all items by storing their index values in the selected items array
+          setSelectedItems([...Array(inputImportedData.length).keys()]);
+        }
+      };
       
       
       
@@ -75,9 +76,6 @@ const ListViewOutput = () => {
         //     setMessage("List is empty")
         // } // Clear the selected items array
       };
-      
-      
-
 
     useEffect(() => {
         if (inputImportedData) {
@@ -86,21 +84,162 @@ const ListViewOutput = () => {
         }
       }, [inputImportedData]);
 
+
+
+      // FILTER FUNCTIONALITY 
+
+      const [selectCarName, setSelectCarName] = useState();
+      const [selectCarModal, setSelectCarModal] = useState();
+      const [selectPurchasedYear, setSelectPurchaseYear] = useState();
+      const [selectFuelType, setSelectFuelType] = useState();
+      const [isVisible, setIsVisible] = useState(false);
+      const [alertType, setAlertType] = useState(0);
+
+      
+
+
+      const handleSelectCarName     = (e) =>{
+        setSelectCarName(e.target.value)
+      }
+      const handleSelectCarModal     =(e) =>{
+        setSelectCarModal(e.target.value)
+      }
+      const handleSelectPurchaseYear =(e) =>{
+        setSelectPurchaseYear(e.target.value)
+      }
+      const handleFuelType           =(e) =>{
+        setSelectFuelType(e.target.value)
+      }
+
+      const handleFilter = () => {
+        let filteredData = [];
+
+        if (inputImportedData && inputImportedData.length) {
+          filteredData = inputImportedData.filter((item) => {
+            if (
+              (!selectCarName || item.Car_Name === selectCarName) &&
+              (!selectCarModal || item.Car_Modal === selectCarModal) &&
+              (!selectPurchasedYear || item.Purchase_Year === selectPurchasedYear) &&
+              (!selectFuelType || item.Fuel_Type === selectFuelType)
+            ) {
+              return true;
+            }
+            return false;
+          });
+          if(filteredData.length>0){
+            setInputImportedData(filteredData);
+            setIsVisible(false);
+          }
+          else {
+            setIsVisible(true);
+            setAlertType(1);
+          }
+        } 
+        else {
+          setIsVisible(true);
+          setAlertType(2);
+        }
+        
+      };
+      
+      const alertMsg = isVisible === true && alertType === 1 && inputImportedData
+  ? <strong>Alert! Items Not Found</strong>
+  : isVisible === true && alertType === 2 
+  ? <strong>Alert! Please Create an Entry First...</strong>
+  : null;
+
+
+      const handleClear = () => {
+        if (selectCarName || selectCarModal || selectFuelType || selectPurchasedYear) {
+          setSelectCarName("");
+          setSelectCarModal("");
+          setSelectFuelType("");
+          setSelectPurchaseYear("");
+        }
+      };
+      
+      const handleClick = () => {
+        setIsVisible(false);
+      };
+
+
+
   return (
     <div>
       <div className="page_container">
         <div className="pagebody">
-            <div className="title_bar">
+          <div className="filter-bar">
+          {isVisible && 
+          <div className="alert">
+            <span className="closebtn" onClick={handleClick} >&times;</span> 
+            {alertMsg}
+          </div>}
+              <div  className="filter-box">
+            	  <div  className="col-md-2 pt-1">
+                   <div  className="form-group ">
+                      <select id="inputState "  className="form-control" value={selectCarName} onChange={handleSelectCarName} >
+                        <option selected >Brand</option>
+                        <option>Toyota</option>
+                        <option>Honda</option>
+                        <option>Ford</option>
+                        <option>Chevrolet</option>
+                        <option>Tesla</option>
+                      </select>
+                   </div>
+                </div>
+                <div  className="col-md-2 pt-1">
+                    <div  className="form-group">
+                       <select id="inputState"  className="form-control" value={selectCarModal} onChange={handleSelectCarModal} >
+                         <option selected>Model</option>
+                         <option>V4</option>
+                         <option>Civic</option>
+                         <option>Mustang</option>
+                         <option>Corvette</option>
+                         <option>Model S</option>
+                       </select>
+                    </div>
+                </div>
+                <div  className="col-md-2 pt-1">
+                     <div  className="form-group">
+                       <select id="inputState"  className="form-control" value={selectPurchasedYear} onChange={handleSelectPurchaseYear} >
+                         <option selected>Purchased Year</option>
+                         <option>2022</option>
+                         <option>2023</option>
+                         <option>2024</option>
+                         <option>2025</option>
+                       </select>
+                     </div>
+                </div>
+                <div  className="col-md-2 pt-1">
+                     <div  className="form-group">
+                       <select id="inputState"  className="form-control" value={selectFuelType} onChange={handleFuelType} >
+                         <option selected>Fuel Type</option>
+                         <option>Electric</option>
+                         <option>Gasoline</option>
+                         <option>Petrol</option>
+                       </select>
+                     </div>
+                </div>
+                <div  className="col-md-0 pt-1">
+            	     <button type="button"  className="btn btn-outline-primary" onClick={handleFilter}>Filter</button>
+            	   </div>
+                 <div  className="col-md-0 pt-1">
+            	     <button type="button"  className="btn btn-outline-danger" onClick={handleClear}>Clear</button>
+            	   </div>
+                  </div>
+                  </div>
+          <div className="title_bar">
                 <ul>
                     <li>
-                        <div class="form-check">
-                        {/* <input
+                        <div  className="form-check">
+                        <input
                            className="form-check-input"
                            type="checkbox"
                            value="all"
                            id="flexCheckDefault"
-                           checked={selectedItems.length === inputImportedData.length}
-                           onChange={handleCheckboxClickAll}/> */}
+                           //checked={selectedItems.length === inputImportedData.length}
+                          onChange={handleCheckboxClickAll}
+                           />
 
                         </div>
                     </li>
@@ -123,8 +262,8 @@ const ListViewOutput = () => {
                  <div className="response_list_bar">
                 <ul>
                     <li>
-                        <div class="form-check">
-                           <input class="form-check-input" type="checkbox" value={index} id="flexCheckDefault" checked={selectedItems.includes(index)}
+                        <div  className="form-check">
+                           <input  className="form-check-input" type="checkbox" value={index} id="flexCheckDefault" checked={selectedItems.includes(index)}
                            onClick={() => handleCheckboxClick(index)}  />
                         </div>
                     </li>
